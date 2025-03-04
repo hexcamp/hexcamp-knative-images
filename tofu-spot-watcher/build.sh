e555f11a2dd26c531ce8770c16bb6833507ab997
Jim Pick <jim@jimpick.com>
2025-03-04 12:47:19 -0800

    Add tofu-spot-watcher

---
+++ tofu-spot-watcher/build.sh
@@ -0,0 +1,1 @@
+docker buildx build --platform linux/amd64 . -t ghcr.io/hexcamp/hexcamp-knative-images/tofu-spot-watcher
---
+++ tofu-spot-watcher/push.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker push ghcr.io/hexcamp/hexcamp-knative-images/tofu-spot-watcher
