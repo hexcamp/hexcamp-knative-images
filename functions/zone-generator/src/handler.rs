87f6e0db7b21888df0da869d8bbd86da81ed0d40
Jim Pick <jim@jimpick.com>
2025-03-17 18:28:14 -0700

    Execute update-zones.sh

--- functions/zone-generator/src/handler.rs
+++ functions/zone-generator/src/handler.rs
@@ -32,19 +32,20 @@ pub async fn handle(event: Event, config: web::Data<HandlerConfig>) -> String {
 
     use std::process::Command;
 
-    let output = Command::new("rustc")
-        .arg("--version")
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("update-zones.sh")
         .output()
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 
     if output.status.success() {
         let s = String::from_utf8_lossy(&output.stdout);
 
-        print!("rustc succeeded and stdout was:\n{}", s);
+        print!("command succeeded and stdout was:\n{}", s);
     } else {
         let s = String::from_utf8_lossy(&output.stderr);
 
-        print!("rustc failed and stderr was:\n{}", s);
+        print!("command failed and stderr was:\n{}", s);
     }
 
     "Event processed".to_owned()
